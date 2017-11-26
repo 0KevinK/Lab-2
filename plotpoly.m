1cb5ec269709a804a2e2e9b8ab0ea5e66f56f09d
0KevinK <33936552+0KevinK@users.noreply.github.com>
2017-11-26 14:44:59 +0100

    Update plotpoly.m

--- plotpoly.m
+++ plotpoly.m
@@ -1,9 +1,7 @@
 function plotpoly(coeffs, points);
-coeffs=[-0.5 4.9 0.5];
-points=[1 5; 2 8;3 11;4 12];
 Xv=points(:,1);
 Yv=points(:,2);
-a=input('Stegl‰ngd fˆr x-v‰rden?');
+a=input('Stegl√§ngd f√∂r x-v√§rden?');
 
 x=[min(Xv):a:max(Xv)];
 A=polyval(coeffs,x);
