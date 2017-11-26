0af9cbd57f73a852d3e0cfd298756ac8cf987a69
0KevinK <33936552+0KevinK@users.noreply.github.com>
2017-11-26 15:14:27 +0100

    Update plotpoly.m

--- plotpoly.m
+++ plotpoly.m
@@ -1,7 +1,7 @@
 function plotpoly(coeffs, points);
 Xv=points(:,1);
 Yv=points(:,2);
-a=input('Steglängd för x-värden?');
+a=input('Steglängd för x-värden? ');
 
 x=[min(Xv):a:max(Xv)];
 A=polyval(coeffs,x);
