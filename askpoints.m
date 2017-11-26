483461d87ace1700aa88fe7d395b840d1ae84e41
0KevinK <33936552+0KevinK@users.noreply.github.com>
2017-11-26 14:21:38 +0100

    Update askpoints.m

--- askpoints.m
+++ askpoints.m
@@ -3,12 +3,12 @@ disp('Ange punkter (retur avslutar inmatningen)')
 points=[];
 A=[];
 
-     A=input('Ange en punkt ([x y]):')
+     A=input('Ange en punkt ([x y]):');
       while size(A) ~= 0 % Om inget gavs i input bryts loopen
-      points=[points;A(1) A(2);] % lägger till det som gavs i input
-      A=input('Ange en punkt ([x y]):')
+      points=[points;A(1) A(2);]; % lÃ¤gger till det som gavs i input
+      A=input('Ange en punkt ([x y]):');
       end
-    %matrisen som ges av detta har x-värden i första kolonnen, med
-    %motsvarande y-värde i vänsta kolonnen
+    %matrisen som ges av detta har x-vÃ¤rden i fÃ¶rsta kolonnen, med
+    %motsvarande y-vÃ¤rde i vÃ¤nsta kolonnen
 
 end
