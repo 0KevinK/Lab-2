9b9e6428f1388eef6fde7a1d9b64e8e468e60fd7
0KevinK <33936552+0KevinK@users.noreply.github.com>
2017-12-06 10:35:56 +0100

    Update askpoints.m

--- askpoints.m
+++ askpoints.m
@@ -3,7 +3,7 @@ disp('Ange punkter (retur avslutar inmatningen)')
 points = [];
 A = [];
 
-A=input('Ange en punkt ([x y]):');
+A = input('Ange en punkt ([x y]):');
       while size(A) ~= 0 % Om inget gavs i input bryts loopen
       points = [points;A(1) A(2);]; % lägger till det som gavs i input
       A = input('Ange en punkt ([x y]):');
