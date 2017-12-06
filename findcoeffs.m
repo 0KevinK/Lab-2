82690cfdcc06173405bb922d3eea3128cbc5c67d
0KevinK <33936552+0KevinK@users.noreply.github.com>
2017-12-06 10:37:23 +0100

    Update findcoeffs.m

--- findcoeffs.m
+++ findcoeffs.m
@@ -1,11 +1,13 @@
 function coeffs = findcoeffs(degree, points);
-N = degree;
-m = length(points(:,1));
+N = degree; %ger polynometsgrad
+m = length(points(:,1)); %ger antalet rader i designmatrisen
 x = points(:,1);
 y = points(:,2);
-A = zeros(m,N+1);
+design = zeros(m,N+1); %skapar en nollmatris som en för designmatrisen
+                       %N+1 då t.ex x^2+x+c ger N=2 men det krävs 3
+                       %kolonner
 for k = 1:N+1
-    A(:,k)=x.^(N+1-k);
+    design(:,k)=x.^(N+1-k);
 end;
-coeffs = A \ y;
+coeffs = design \ y; %Gauss-Jordan elimination för att lösa koefficienter
 end
