03e0e81b49432af56504fa28e14b75ce003312b6
EttNollEtt <33880192+EttNollEtt@users.noreply.github.com>
2017-12-06 12:00:49 +0100

    Update plotpoly.m
    Ändrade så s blev noggranheten/antalet x-punkter som polynomet ritas av och nu går det inte att förstöra den genom att ge ett konstigt värde

--- plotpoly.m
+++ plotpoly.m
@@ -1,12 +1,15 @@
-function plotpoly(coeffs, points);
-xV = points(:,1); % x-värden är i points första kolonn
+function plotpoly(coeffs, points)
+xV = points(:,1); % x-vÃ¤rden Ã¤r i points fÃ¶rsta kolonn
 yV = points(:,2);
-s = input('Steglängd för x-värden? ');
 
-polyX = [min(xV):s:max(xV)]; %ger x värden att beräkna
-polyY = polyval(coeffs,polyX); %beräknar ovanstående punkter
+s = input('Nogrannhet i grafen? ');
 
-plot(polyX,polyY) % plottar grafen som ges av polynomet
+steg = (max(xV)-min(xV))/s;
+
+polyX = (min(xV):steg:max(xV)); %ger x vÃ¤rden att berÃ¤kna
+polyY = polyval(coeffs, polyX); %berÃ¤knar ovanstÃ¥ende punkter
+
+plot(polyX, polyY) % plottar grafen som ges av polynomet
 hold on
-plot(xV,yV,'+') %plottar in punkterna som gavs av användaren
+plot(xV, yV, '+') %plottar in punkterna som gavs av anvÃ¤ndaren
 end
