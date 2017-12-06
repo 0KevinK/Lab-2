5555afbfc3010f7dfd9205fc6c30274193bf36e5
EttNollEtt <33880192+EttNollEtt@users.noreply.github.com>
2017-12-06 14:53:12 +0100

    Update findcoeffs.m (#6)
    Ännu lite mer noggrannare variabler. Fixade lite läsbarhet med mellanslag

--- findcoeffs.m
+++ findcoeffs.m
@@ -1,13 +1,13 @@
-function coeffs = findcoeffs(degree, points);
-N = degree; %ger polynometsgrad
-m = length(points(:,1)); %ger antalet rader i designmatrisen
-x = points(:,1);
-y = points(:,2);
-design = zeros(m,N+1); %skapar en nollmatris som en för designmatrisen
-                       %N+1 då t.ex x^2+x+c ger N=2 men det krävs 3
+function coeffs = findcoeffs(degree, points)
+nyDegree = degree + 1; %ger polynometsgrad
+rader = length(points(:, 1)); %ger antalet rader i designmatrisen
+x = points(:, 1);
+y = points(:, 2);
+design = zeros(rader,nyDegree); %skapar en nollmatris som en fÃ¶r designmatrisen
+                       %N+1 dÃ¥ t.ex x^2+x+c ger N=2 men det krÃ¤vs 3
                        %kolonner
-for k = 1:N+1
-    design(:,k)=x.^(N+1-k);
-end;
-coeffs = design \ y; %Gauss-Jordan elimination för att lösa koefficienter
+for k = 1:nyDegree
+    design(:, k)=x.^(nyDegree-k);
+end
+coeffs = design \ y; %Gauss-Jordan elimination fÃ¶r att lÃ¶sa koefficienter
 end
