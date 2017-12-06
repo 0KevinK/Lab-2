8ccb4b34bce18d60ef0a1c9177237e6fceb2d67b
EttNollEtt <33880192+EttNollEtt@users.noreply.github.com>
2017-12-06 11:59:16 +0100

    Update askpoints.m
    Lite indenteringar

--- askpoints.m
+++ askpoints.m
@@ -4,11 +4,11 @@ points = [];
 punkt = [];
 
 punkt = input('Ange en punkt ([x y]):');
-      while size(punkt) ~= 0 % Om inget gavs i input bryts loopen
-      points = [points;punkt(1) punkt(2);]; % lägger till det som gavs i input
-      punkt = input('Ange en punkt ([x y]):');
-      end
-     %matrisen som ges av detta har x-värden i första kolonnen, med
-     %motsvarande y-värde i vänsta kolonnen
+while size(punkt) ~= 0 % Om inget gavs i input bryts loopen
+    points = [points;punkt(1) punkt(2);]; % lÃ¤gger till det som gavs i input
+    punkt = input('Ange en punkt ([x y]):');
+end
+     %matrisen som ges av detta har x-vÃ¤rden i fÃ¶rsta kolonnen, med
+     %motsvarande y-vÃ¤rde i vÃ¤nsta kolonnen
 
 end
