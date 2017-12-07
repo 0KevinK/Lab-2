b8a632bcbd96cbf70689cb84710c34811da572e3
0KevinK <33936552+0KevinK@users.noreply.github.com>
2017-12-07 16:54:39 +0100

    Update plotpoly.m

--- plotpoly.m
+++ plotpoly.m
@@ -1,13 +1,13 @@
 function plotpoly(coeffs, points)
-xV = points(:,1); % x-vÃ¤rden Ã¤r i points fÃ¶rsta kolonn
+xV = points(:,1); % x-värden är i points första kolonn
 yV = points(:,2);
 
 s = input('Nogrannhet i grafen? ');
 
 steg = (max(xV)-min(xV))/s;
 
-polyX = (min(xV):steg:max(xV)); %ger x vÃ¤rden att berÃ¤kna
-polyY = polyval(coeffs, polyX); %berÃ¤knar ovanstÃ¥ende punkter
+polyX = (min(xV):steg:max(xV)); %ger x värden att beräkna
+polyY = polyval(coeffs, polyX); %beräknar ovanstående punkter
 
 plot(polyX, polyY) % plottar grafen som ges av polynomet
 hold on
