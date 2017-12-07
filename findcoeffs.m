2ce0eaa99dd4410cc5a46115c6de3fc2d1dcfcfa
0KevinK <33936552+0KevinK@users.noreply.github.com>
2017-12-07 16:53:45 +0100

    Update findcoeffs.m

--- findcoeffs.m
+++ findcoeffs.m
@@ -3,9 +3,8 @@ nyDegree = degree + 1; %ger polynometsgrad
 rader = length(points(:, 1)); %ger antalet rader i designmatrisen
 x = points(:, 1);
 y = points(:, 2);
-design = zeros(rader,nyDegree); %skapar en nollmatris som en fÃ¶r designmatrisen
-                       %N+1 dÃ¥ t.ex x^2+x+c ger N=2 men det krÃ¤vs 3
-                       %kolonner
+design = zeros(rader,nyDegree); %skapar en nollmatris som en grund för designmatrisen
+
 for k = 1:nyDegree
     design(:, k)=x.^(nyDegree-k);
 end
